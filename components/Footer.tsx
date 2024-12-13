421e5db25971d5107b722c9a242c42652b134010
Yousef Raeis <144580894+yousefraeis@users.noreply.github.com>
2024-12-13 23:52:20 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
                   Reach out to me today and let&apos;s discuss how I can help
                   you achieve your goals.
               </p>
-              <a href='mailto:yousefraeis@outlook.com'>
+              <a href='mailto:raeis.pro@gmail.com'>
                   <MagicButton
                       title="Let's get in touch"
                       icon={<FaLocationArrow />}
